fe2e21b79a6c42cfeee4b9ea6b74b2243d340633
Parth Zile <parthzile01@gmail.com>
2023-12-18 10:34:11 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,28 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  const data = imageData()
+
+  // console.log()        
+  return(
+    <div className='container'>
+      <div>
+        <img src={data[0].img} alt="" id={data[0].id}/>
+      </div>
+
+      <div>
+        <img src={data[1].img} alt="" id ={data[1].id}/>        
+      </div>
+
+      <div>
+        <img src={data[2].img} alt="" id ={data[2].id}/>        
+      </div>
+
+      <div>
+        <img src={data[3].img} alt="" id ={data[3].id}/>        
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -26,5 +26,23 @@ export default class AppClass extends Component{
     return data;
   }
 
-  // code here
+  render(){
+    const data = this.imageData()
+    return (
+      <div className="container">
+        <div>
+          <img src={data[0].img} alt="" id={data[0].id}/>
+        </div>
+        <div>
+          <img src={data[1].img} alt="" id={data[1].id}/>
+        </div>
+        <div>
+          <img src={data[2].img} alt="" id={data[2].id}/>
+        </div>
+        <div>
+          <img src={data[3].img} alt="" id={data[3].id}/>
+        </div>
+      </div>
+    )
+  }
 }
