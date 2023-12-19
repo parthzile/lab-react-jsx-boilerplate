370943e2981621c89506c5b47597af355c7d9ad2
Parth Zile <parthzile01@gmail.com>
2023-12-19 14:06:17 +0530

    messed up

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,14 @@
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
 
-function App() {
+function App(Imgdata) {
   // code here
-  const data = imageData()
 
-  // console.log()        
+  console.log(Imgdata)
+      
   return(
-    <div className='container'>
-      <div>
-        <img src={data[0].img} alt="" id={data[0].id}/>
-      </div>
-
-      <div>
-        <img src={data[1].img} alt="" id ={data[1].id}/>        
-      </div>
-
-      <div>
-        <img src={data[2].img} alt="" id ={data[2].id}/>        
-      </div>
-
-      <div>
-        <img src={data[3].img} alt="" id ={data[3].id}/>        
-      </div>
-    </div>
+    <div></div>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import AppClass from './AppClass';
-// import AppClass from "./AppClass";
+import DataComponent from '../../exercise-lab-props-boilerplate/src/components/Image';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppClass />   
+    <App dataImg={DataComponent}/>   
     {/* change the above line - for interchaning between App and AppClass */}
   </React.StrictMode>
 );
